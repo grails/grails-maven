84413251dc84c174e295e7a665166bf06e99b291
Jonathan Pearlin <jpearlin1@gmail.com>
2014-05-05 08:36:17 -0400

    Remove extra call to TestApp.

--- src/main/java/org/grails/maven/plugin/GrailsTestAppMojo.java
+++ src/main/java/org/grails/maven/plugin/GrailsTestAppMojo.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -48,7 +48,6 @@ public class GrailsTestAppMojo extends AbstractGrailsMojo {
         if(getEnvironment() == null) {
             env = "test";
         }
-        runGrails("TestApp");
 
         String args = null;
 
@@ -60,6 +59,6 @@ public class GrailsTestAppMojo extends AbstractGrailsMojo {
             args = (args != null) ? args + " " + testPatterns : testPatterns;
         }
 
-        runGrails("TestApp", args);
+        runGrails("TestApp", args != null ? args : System.getProperty("grails.cli.args"));
     }
 }
